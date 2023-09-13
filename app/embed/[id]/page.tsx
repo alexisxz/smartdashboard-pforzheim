cf09571c99d7e0bdcb8712a75c1ee8dca5484cff
Felix Erdmann <hello@felixerdmann.com>
2023-09-13 13:28:31 +0200

    feat: add logo below iframe embed

--- app/embed/[id]/page.tsx
+++ app/embed/[id]/page.tsx
@@ -1,5 +1,9 @@
+import Title from '@/components/Elements/Title'
 import TileFactory, { TileType } from '@/utils/TileFactory'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
+import MSLogo from '@/assets/logos/stadtlogo-muenster.png'
+import Image from 'next/image'
 
 export const revalidate = 10
 
@@ -10,5 +14,24 @@ export default async function Embed({ params }: { params: { id: TileType } }) {
     return notFound()
   }
 
-  return <TileFactory type={id} />
+  return (
+    <div>
+      <TileFactory type={id} />
+      <div className="mt-4 flex h-full w-full items-center justify-end gap-4">
+        <Image
+          alt="Logo der Stadt Münster"
+          className="h-10 w-auto"
+          src={MSLogo}
+        />
+        <Title as="h7" className="leading-normal" variant={'primary'}>
+          mehr Infos zu den Klimadaten aus Münster
+          <br />
+          findet ihr auf{' '}
+          <Link className="underline" href="">
+            klimadashboard.ms
+          </Link>
+        </Title>
+      </div>
+    </div>
+  )
 }
