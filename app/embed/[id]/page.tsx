80ed7ac36339639abfe56f9e5332f61ab38bc198
Felix Erdmann <hello@felixerdmann.com>
2023-09-13 13:32:31 +0200

    feat: mobile friendls iframe logo

--- app/embed/[id]/page.tsx
+++ app/embed/[id]/page.tsx
@@ -17,17 +17,21 @@ export default async function Embed({ params }: { params: { id: TileType } }) {
   return (
     <div>
       <TileFactory type={id} />
-      <div className="mt-4 flex h-full w-full items-center justify-end gap-4">
+      <div className="mt-4 flex h-full w-full flex-col justify-end gap-4 md:flex-row md:items-center">
         <Image
           alt="Logo der Stadt Münster"
-          className="h-10 w-auto"
+          className="h-10 w-fit"
           src={MSLogo}
         />
         <Title as="h7" className="leading-normal" variant={'primary'}>
           mehr Infos zu den Klimadaten aus Münster
           <br />
           findet ihr auf{' '}
-          <Link className="underline" href="">
+          <Link
+            className="underline"
+            href="https://klimadashboard.ms/"
+            target="_blank"
+          >
             klimadashboard.ms
           </Link>
         </Title>
