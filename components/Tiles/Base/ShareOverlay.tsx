201ea15c69ea3a158cad8193c5ab69e2847516d8
Umut tas <u.tas@reedu.de>
2023-09-28 13:15:50 +0000

    Fix overlays

--- components/Tiles/Base/ShareOverlay.tsx
+++ components/Tiles/Base/ShareOverlay.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import AnimatedCopyIcon from '@/components/Elements/Animated/AnimatedCopyIcon'
-import { Spacer } from '@/components/Elements/Spacer'
 import Title from '@/components/Elements/Title'
 import { ComponentPropsWithRef } from 'react'
 import { AnimatedProps } from '@react-spring/web'
 import BaseOverlay from './BaseOverlay'
 import { TileType } from '@/utils/TileFactory'
+import { Button } from '@/components/Elements/Button'
+import { ClipboardDocumentIcon } from '@heroicons/react/24/outline'
 
 type ShareOverlayProps = AnimatedProps<ComponentPropsWithRef<'div'>> & {
   onClose?: () => void
@@ -26,18 +27,40 @@ export default function ShareOverlay({
 
   return (
     <BaseOverlay onClose={onClose} {...props}>
-      <div className="flex h-full w-full flex-col">
-        <Title as="h3" variant={'secondary'}>
-          Diese Kachel teilen
-        </Title>
-        <Spacer />
-        <div className="flex w-full rounded bg-white p-4">
-          <pre className="m-4 flex-1 whitespace-pre-wrap break-all text-sm">
-            {link}
-          </pre>
-          <div className="relative w-7">
-            <AnimatedCopyIcon onClick={copyToClipboard} />
+      <div className="flex h-full w-full flex-1 flex-col">
+        <div className="grid grid-flow-col grid-cols-3 grid-rows-2 gap-8">
+          <div className="col-span-2 row-span-1">
+            <Title as="h3" variant={'secondary'}>
+              Diese Kachel teilen
+            </Title>
           </div>
+          <div className="col-span-2 row-span-1">
+            <div className="flex w-full rounded bg-white p-4">
+              <pre className="m-4 flex-1 whitespace-pre-wrap break-all text-sm">
+                {link}
+              </pre>
+              <div className="relative w-7">
+                <AnimatedCopyIcon onClick={copyToClipboard} />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1 row-span-2">
+            <p className="text-white">
+              Sie möchten diese Kachel mit anderen teilen? Nutzen Sie diesen
+              Link um direkt zu dieser Kachel zu verlinken.
+            </p>
+          </div>
+        </div>
+        <div className="pt-10">
+          <Button
+            onClick={copyToClipboard}
+            startIcon={
+              <ClipboardDocumentIcon className="w-5 stroke-2 text-secondary" />
+            }
+            variant={'overlay'}
+          >
+            Link kopieren
+          </Button>
         </div>
       </div>
     </BaseOverlay>
