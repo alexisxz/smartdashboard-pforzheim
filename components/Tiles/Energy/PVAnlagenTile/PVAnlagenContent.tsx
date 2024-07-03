a6af1b17e36c40b371c6f139debe7714a153bc1c
Felix Erdmann <hello@felixerdmann.com>
2024-07-03 14:40:54 +0200

    feat: update pv tile

--- components/Tiles/Energy/PVAnlagenTile/PVAnlagenContent.tsx
+++ components/Tiles/Energy/PVAnlagenTile/PVAnlagenContent.tsx
@@ -5,15 +5,35 @@ import Title from '@/components/Elements/Title'
 
 // @ts-ignore
 import PVData from '@/assets/data/pv-anlagen.csv'
+import { Spacer } from '@/components/Elements/Spacer'
 import { MsKlimadashboardIconsEPvGebaeude } from '@/components/Icons/Energie'
+import Slider from '@/components/Inputs/Slider'
+import { useState } from 'react'
 import { PVAnlagenDataType } from '.'
 
 export default function PVAnlagenContent() {
   const allData = PVData as PVAnlagenDataType[]
-  const data = allData.at(-1)!
+
+  // find all data where data['Leistung aller Anlagen (Summe)'] and data['Anzahl aller PV-Anlagen'] is not null
+  const filteredData = allData
+    .filter(
+      e =>
+        e['Leistung aller Anlagen (Summe)'] !== null &&
+        e['Anzahl aller PV-Anlagen'] !== null,
+    )
+    .sort((a, b) => a.ZEIT - b.ZEIT)
+
+  const [yearIndex, setYearIndex] = useState(filteredData.length - 1)
 
   return (
     <div>
+      <Title as={'h1'} className="min-w-fit text-energy" font={'normal'}>
+        <AnimatedNumber>
+          {filteredData[yearIndex]['Anzahl aller PV-Anlagen']}
+        </AnimatedNumber>{' '}
+        PV-Anlagen
+      </Title>
+      <Spacer />
       <div className="mb-4 flex flex-row gap-6">
         <span>
           <MsKlimadashboardIconsEPvGebaeude className="h-20 text-energy md:h-32" />
@@ -24,13 +44,25 @@ export default function PVAnlagenContent() {
             entspricht einer Leistung von{' '}
             <span className="text-energy">
               <AnimatedNumber decimals={0}>
-                {data['Leistung aller Anlagen (Summe)']}
+                {filteredData[yearIndex]['Leistung aller Anlagen (Summe)']}
               </AnimatedNumber>{' '}
               kWp.
             </span>{' '}
           </Title>
         </div>
       </div>
+      <Spacer />
+      <Slider
+        defaultValue={[filteredData.at(-1)!.ZEIT]}
+        firstValueMobile={filteredData.at(0)!.ZEIT}
+        labels={filteredData.map(e => e.ZEIT.toString())}
+        max={filteredData.length - 1}
+        min={0}
+        onValueChange={([e]) => {
+          setYearIndex(e)
+        }}
+        variant={'energy'}
+      />
     </div>
   )
 }
