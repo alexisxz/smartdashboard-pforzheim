7a527291a9558c774412cd8184c26ceb85dcc982
alexisxz <alexisxzinho@hotmail.com>
2025-07-15 07:35:00 -0300

    fix: typo

--- components/Tiles/Mobility/Bicycle/Stadtradeln/index.tsx
+++ components/Tiles/Mobility/Bicycle/Stadtradeln/index.tsx
@@ -17,7 +17,7 @@ export default async function StadtradelnTile() {
       <Spacer />
       {/* <Title as="h5">{infoText}</Title> */}
       <Title as="h5">
-        Auf die Leeze, fertig, los! Für mehr Radverkehr, Klimaschutz und
+        Auf die Plätze, fertig, los! Für mehr Radverkehr, Klimaschutz und
         Lebensqualität in die Pedale treten: Seit 2020 beteiligt sich die Stadt
         Pforzheim an der jährlich stattfindenden, dreiwöchigen bundesweiten
         Aktion „STADTRADELN“ des globalen Netzwerks „Klima-Bündnis“.
